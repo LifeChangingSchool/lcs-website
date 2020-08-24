b3f1103d7e8558bab71daa80a431db305f79b9f7
Samson Zhang <wwsamson@yahoo.com>
2020-08-24 01:03:29 -0400

    fix stripe redirect

--- pages/api/AppOct2020/stripeCheckout.ts
+++ pages/api/AppOct2020/stripeCheckout.ts
@@ -25,8 +25,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             ],
             mode: "payment",
             customer_email: req.body,
-            success_url: process.env.NODE_ENV === "production" ? "https://peaceful-brahmagupta-7beb96.netlify.app/?stage=2" : "http://localhost:3000/apply?stage=2",
-            cancel_url: "production" ? "https://peaceful-brahmagupta-7beb96.netlify.app/?stage=2" : "http://localhost:3000",
+            success_url: process.env.NODE_ENV === "production" ? "https://peaceful-brahmagupta-7beb96.netlify.app/apply?stage=2" : "http://localhost:3000/apply?stage=2",
+            cancel_url: "production" ? "https://peaceful-brahmagupta-7beb96.netlify.app/apply?stage=2" : "http://localhost:3000",
         });
 
         res.status(200).json({id: session.id});
