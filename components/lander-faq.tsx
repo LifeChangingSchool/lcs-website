b4de422b8aba654744462a9ca19648533ca81ba2
Samson Zhang <wwsamson@yahoo.com>
2020-08-31 17:35:07 -0400

    fix lander FAQs

--- components/lander-faq.tsx
+++ components/lander-faq.tsx
@@ -2,11 +2,11 @@ import Accordion from "react-robust-accordion";
 
 const LanderFAQ = (props: {question: string, answer: string}) => (
     <Accordion label={(
-            <p>What are you looking for in your applicants?</p>
+            <p>{props.question}</p>
     )} open={false} className="p-4 border">
         <div>
             <hr/>
-            <p className="text-xs">The admissions team is looking for talented, highly-motivated students who are interested in attending the program because they have a passion for learning and problem-solving and wish to enrich their knowledge of programming and entrepreneurship to solve real world problems.</p>
+            <p className="text-xs">{props.answer}</p>
         </div>
     </Accordion>
 )
