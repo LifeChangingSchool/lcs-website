1572afce940afcf9952bef42e019c98e23a21874
Samson Zhang <wwsamson@yahoo.com>
2020-09-01 09:57:10 -0400

    add facebook pixel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,15 +10,39 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;900&family=Rubik:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;900&family=Rubik:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap"
+                        rel="stylesheet"/>
                     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177009903-1"></script>
-                    <script dangerouslySetInnerHTML={{__html: `
+                    <script dangerouslySetInnerHTML={{
+                        __html: `
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
-
-                        gtag('config', 'UA-177009903-1');
-                    `}}/>
+                        gtag('config', 'UA-177009903-1', {
+                          page_path: window.location.pathname,
+                        });
+                    `
+                    }}/>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                            n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)}(window,document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '1761498877395738');
+                        fbq('track', 'PageView');                    
+                    `
+                    }}>
+                    </script>
+                    <noscript>
+                        <img height="1" width="1"
+                             src="https://www.facebook.com/tr?id=1761498877395738&ev=PageView&noscript=1"/>
+                    </noscript>
                 </Head>
                 <body>
                 <Main/>
